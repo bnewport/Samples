c7b1fc4bf54617daead032926eabec4a8e30b37c
Billy Newport <bnewport@mac.com>
2010-08-05 15:41:29 -0500

    Updated to use new wxsutils

--- redis-wxs/target/classes/clear.sql
+++
@@ -1,8 +0,0 @@
-delete from db2admin.setheadstringlong;
-delete from db2admin.setitemstringlong;
-delete from db2admin.stringstringproperty;
-delete from db2admin.stringlongproperty;
-delete from db2admin.longlongproperty;
-delete from db2admin.longstringproperty;
-delete from db2admin.listheadstringlong;
-delete from db2admin.listitemstringlong;
