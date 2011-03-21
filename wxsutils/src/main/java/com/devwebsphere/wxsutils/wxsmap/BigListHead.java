d214cfa8346ab08dc2f1f60070bd13a0a90b0160
Billy Newport <bnewport@mac.com>
2011-03-21 10:39:13 -0500

    Adding missing license headers

--- wxsutils/src/main/java/com/devwebsphere/wxsutils/wxsmap/BigListHead.java
+++ wxsutils/src/main/java/com/devwebsphere/wxsutils/wxsmap/BigListHead.java
@@ -1,3 +1,13 @@
+//
+//This sample program is provided AS IS and may be used, executed, copied and
+//modified without royalty payment by customer (a) for its own instruction and 
+//study, (b) in order to develop applications designed to run with an IBM 
+//WebSphere product, either for customer's own internal use or for redistribution 
+//by customer, as part of such an application, in customer's own products. "
+//
+//5724-J34 (C) COPYRIGHT International Business Machines Corp. 2005
+//All Rights Reserved * Licensed Materials - Property of IBM
+//
 package com.devwebsphere.wxsutils.wxsmap;
 
 import java.io.Serializable;
