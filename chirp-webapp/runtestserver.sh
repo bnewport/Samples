0479a5c363932c806dc547e85f6033c1e7ab4798
Billy Newport <bnewport@mac.com>
2011-03-23 10:14:29 -0500

    Classes moved

--- chirp-webapp/runtestserver.sh
+++ chirp-webapp/runtestserver.sh
@@ -1,2 +1,2 @@
-java -cp "target/chirp-webapp/WEB-INF/lib/*" $MAVEN_OPTS com.devwebsphere.wxsutils.StartSingleJVMGrid Grid /objectgrid_jdbc_writethrough.xml /deployment.xml
+java -cp "target/chirp-webapp/WEB-INF/lib/*" $MAVEN_OPTS com.devwebsphere.wxsutils.wxsmap.StartSingleJVMGrid Grid /objectgrid_jdbc_writethrough.xml /deployment.xml
 
