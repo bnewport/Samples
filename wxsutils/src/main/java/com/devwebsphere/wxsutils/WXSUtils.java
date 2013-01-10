d75bfa863ad73a430507144f4f3529751722c565
fraenkel <fraenkel@us.ibm.com>
2013-01-10 11:18:01 -0500

    Remove deprecated exceptions

--- wxsutils/src/main/java/com/devwebsphere/wxsutils/WXSUtils.java
+++ wxsutils/src/main/java/com/devwebsphere/wxsutils/WXSUtils.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.SortedMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
